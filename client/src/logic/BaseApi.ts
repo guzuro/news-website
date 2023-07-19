de5831a79aae7ea3c69dfed5fbe64b99f55ef826
guzuro <guzurogmn@gmail.com>
2023-07-19 23:57:41 +0300

    url change nodejs 18

--- client/src/logic/BaseApi.ts
+++ client/src/logic/BaseApi.ts
@@ -1,6 +1,5 @@
 export async function baseApi(resource: string, params: Record<string, string>) {
-  const BASE_API_URL = 'http://localhost:1337/api/'
-
+  const BASE_API_URL = 'http://127.0.0.1:1337/api/'
   const response = await fetch(BASE_API_URL + resource + '?' + new URLSearchParams(params))
 
   return response.json().then((j) => j.data)
