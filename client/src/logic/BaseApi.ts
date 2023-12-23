640ad5afd47a1cea3d21f9e352117bfea1c0fd0d
guzuro <guzurogmn@gmail.com>
2023-12-24 01:27:29 +0300

    generic

--- client/src/logic/BaseApi.ts
+++ client/src/logic/BaseApi.ts
@@ -1,6 +1,6 @@
-export async function baseApi(resource: string, params: Record<string, string>) {
+export async function baseApi<T = any>(resource: string, params: Record<string, string>) {
   const BASE_API_URL = 'http://127.0.0.1:1337/api/'
   const response = await fetch(BASE_API_URL + resource + '?' + new URLSearchParams(params))
 
-  return response.json().then((j) => j.data)
+  return response.json().then<T>((j) => j.data)
 }
