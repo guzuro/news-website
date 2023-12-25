28402ea273b90a708fff295e2674630b44cff238
guzuro <guzurogmn@gmail.com>
2023-12-25 23:41:08 +0300

    beautify

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   redirects: {
     "/": "/articles/1",
-    "/articles": "/articles/1"
-  }
-})
+    "/articles": "/articles/1",
+  },
+});
--- client/src/logic/BaseApi.ts
+++ client/src/logic/BaseApi.ts
@@ -1,6 +1,11 @@
-export async function baseApi<T = any>(resource: string, params: Record<string, string>) {
-  const BASE_API_URL = 'http://127.0.0.1:1337/api/'
-  const response = await fetch(BASE_API_URL + resource + '?' + new URLSearchParams(params))
+export async function baseApi<T = any>(
+  resource: string,
+  params: Record<string, string>
+) {
+  const BASE_API_URL = "http://127.0.0.1:1337/api/";
+  const response = await fetch(
+    BASE_API_URL + resource + "?" + new URLSearchParams(params)
+  );
 
-  return response.json().then<T>((j) => j.data)
+  return response.json().then<T>(j => j.data);
 }
