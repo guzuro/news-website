39fad14b354da297ddfc611b7be0ea760f1c5d07
guzuro <guzurogmn@gmail.com>
2023-12-28 23:47:28 +0300

    sitemap deps

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "http://localhost:4321/",
+  integrations: [tailwind(), sitemap()],
   redirects: {
     "/": "/articles/1",
     "/articles": "/articles/1",
